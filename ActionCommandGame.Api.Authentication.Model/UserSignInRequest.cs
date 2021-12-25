7bda8c43f3ccb415f3fb46f068b20b6989415b83
koent <koent@LAPTOP>
2021-12-25 17:20:11 +0100

    adjusted general styling

--- ActionCommandGame.Api.Authentication.Model/UserSignInRequest.cs
+++ ActionCommandGame.Api.Authentication.Model/UserSignInRequest.cs
@@ -4,8 +4,10 @@ namespace ActionCommandGame.Api.Authentication.Model
 {
 	public class UserSignInRequest
 	{
+		[Required(ErrorMessage = "Please use a valid email")]
 		public string? Email { get; set; }
 
-		public string? Password { get; set; }
+        [Required(ErrorMessage = "Please use the correct password")]
+        public string? Password { get; set; }
 	}
 }
