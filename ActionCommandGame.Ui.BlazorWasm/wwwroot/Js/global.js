2fd9a1127cf9a33d0c8be5e87fb9fa027d2e136c
koent <koent@LAPTOP>
2021-12-27 22:19:17 +0100

    Added sounds with login and logout

--- ActionCommandGame.Ui.BlazorWasm/wwwroot/Js/global.js
+++ ActionCommandGame.Ui.BlazorWasm/wwwroot/Js/global.js
@@ -15,6 +15,26 @@
     }
 }
 
-window.PlaySound = function() {
-    document.getElementById('sound').play();
+//window.PlaySound = function() {
+//    document.getElementById('sound').play();
+//}
+
+//window.PlaySound = function() {
+//    document.getElementById('WhoIsIt').play();
+//}
+
+//window.PlaySound = function() {
+//    document.getElementById('GameOver').play();
+//}
+
+window.PlayAudioFile = (src) => {
+    var audio = document.getElementById('player');
+    if (audio != null) {
+        var audioSource = document.getElementById('playerSource');
+        if (audioSource != null) {
+            audioSource.src = src;
+            audio.load();
+            audio.play();
+        }
+    }
 }
