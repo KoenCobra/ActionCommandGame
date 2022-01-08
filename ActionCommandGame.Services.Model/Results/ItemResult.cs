e87ce63e245886b7f35350067436701a7b673309
koent <koent@LAPTOP>
2022-01-08 22:21:25 +0100

    adde styling and required att

--- ActionCommandGame.Services.Model/Results/ItemResult.cs
+++ ActionCommandGame.Services.Model/Results/ItemResult.cs
@@ -1,16 +1,25 @@
-﻿namespace ActionCommandGame.Services.Model.Results
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActionCommandGame.Services.Model.Results
 {
     public class ItemResult
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
-
+        [Required]
         public int Price { get; set; }
+        [Required]
         public int Fuel { get; set; }
+        [Required]
         public int Attack { get; set; }
+        [Required]
         public int Defense { get; set; }
+        [Required]
         public int ActionCooldownSeconds { get; set; }
     }
 }
